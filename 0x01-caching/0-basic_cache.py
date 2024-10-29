ab26403e7ef36cee9622c43bbbaf5fd8a13c99df
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-29 19:09:41 +0100

    task: Basic caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Basic dictionary module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class for basic caching
+    """
+
+    def put(self, key, item):
+        """Assigns an item to a key
+        """
+        if key is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns the value of the key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
